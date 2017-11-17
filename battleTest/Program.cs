4b246b942dafec61d93ead16231329593cbd4c8a
Alexander Samson <alexander@gm7.nl>
2017-11-17 12:17:51 +0100

    None

--- battleTest/Program.cs
+++ battleTest/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-namespace battleTest
+namespace BlueSeaBattle
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Challenge challenge = new Challenge();
+            challenge.GetChallenge();
         }
     }
 }
